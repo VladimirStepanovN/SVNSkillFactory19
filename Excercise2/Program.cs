f3431ef5fadc404f7e3665be8c9810bea389cf1d
VladimirStepanovN <Vladimir.Stepanov.Dev@yandex.ru>
2023-06-28 20:02:41 +0500

    Добавлен проект Excercise2.

---
+++ Excercise2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Excercise2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
